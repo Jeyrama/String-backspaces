1d9309f798f6cc6ed0a1ad45fe1014aa6cf7d54d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-12 18:52:55 -0800

    Update String-backspaces.js

--- String-backspaces.js
+++ String-backspaces.js
@@ -26,3 +26,9 @@ function clean_string(s) {
 }
 
 // or
+
+function cleanString(s) {
+  let result = [];
+  [...s].map((char) => (char === "#" ? result.pop() : result.push(char)));
+  return result.join("");
+}
