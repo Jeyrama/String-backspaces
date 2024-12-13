e5f5ccd4fd1fc20d6057dd90e3e23ed523f6754a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-12 18:52:37 -0800

    Update String-backspaces.js

--- String-backspaces.js
+++ String-backspaces.js
@@ -12,3 +12,15 @@ Examples:
 
 
 // Solution
+
+function clean_string(s) {
+  const result = []
+	for (const c of s) {
+    if (c === "#") {
+      result.pop()
+    } else {
+      result.push(c)
+    }
+  }
+  return result.join("")
+}
